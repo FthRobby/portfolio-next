4ee1c70187a82c2d970e6c265e2ca0e517bae662
FthRobby <rbysalam10@gmail.com>
2023-07-22 17:33:56 +0700

    🎨 init : first init projects

---
+++ utils/data.ts
@@ -0,0 +1,125 @@
+import { FiGithub } from "react-icons/fi";
+import { TbBrandTelegram } from "react-icons/tb";
+import { FiLinkedin } from "react-icons/fi";
+import { AiOutlineMail } from "react-icons/ai";
+import Quran from "../public/quran.png";
+import Smartfarm from '../public/smartfarm.png'
+import SmartPoS from '../public/smartpos.png'
+import { Experience, List,  Social } from "./model";
+
+export const list: List[] = [
+  {
+    link: "about",
+  },
+  {
+    link: "experience",
+  },
+  {
+    link: "work",
+  },
+  {
+    link: "contact",
+  },
+];
+export const experience: Experience[] = [
+  {
+    id: 0,
+    title: "PT. Time Excelindo",
+    date: "Present",
+    story:
+      "Working with team, design and build web application for customer using frontend framework Vue JS, and consume API from Backend.",
+  },
+  {
+    id: 1,
+    title: "PT. Git Solution",
+    date: "May 2023",
+    story:
+      "Im working with team developer, build interactive point of sales as interactive web, mainly focused on building applications for customer food orders, and displaying payment methods for customer menu payments using Vue JS",
+  },
+  {
+    id: 2,
+    title: "Freelance",
+    date: "December 2021",
+    story:
+    "I have experience working as a freelance web developer, where I helped clients create websites to display online food menus. This experience has taught me the importance of attention to detail and the ability to work closely with clients to ensure their vision is realized.",
+  },
+];
+export const social: Social[] = [
+  {
+    icon: FiGithub,
+    link: "https://github.com/fthrobby",
+    title: "Github",
+  },
+  {
+    icon: FiLinkedin,
+    link: "https://www.linkedin.com/in/fatah-robbysalam/",
+    title: "Linkedin",
+  },
+  {
+    icon: AiOutlineMail,
+    email: "rbysalam10@gmail.com",
+    link: "mailto:rbysalam10@gmail.com",
+    title: "Email",
+  },
+];
+
+export const project = [
+  {
+    title: "SmartPoS Dashboard",
+    desc: "Build dashoard for SmartPoS, this dashboard is used to monitor sales, and manage products, and manage users. This dashboard is built using Vue JS, and Bootstrap-vue.",
+    tech: ["Vue", "Vuex", "Bootstrap-vue"],
+    link: "#",
+    image: SmartPoS,
+  },
+  {
+    title: "Smartfarm Dashboard",
+    desc: "Build dashboard form Smartfarm, this dashboard is used to monitor the condition of animals, and monitor the condition of the farm. This dashboard is built using Vue JS, and Bootstrap-vue.",
+    tech: ["Vue", "Vuex", "Bootstrap-vue", "Firebase"],
+    link: "",
+    image: Smartfarm,
+    live: "https://smartfarm.itani.id",
+  },
+  {
+    title: "Alquran-Online",
+    desc: "Al-Quran application online, this application can make it easier for users to be able to read the Quran anywhere and anytime.",
+    tech: ["Vue", "Tailwind", "Netlify"],
+    link: "https://github.com/Zeddnyx/timeTrackingDashboard",
+    image: Quran,
+    // live: "https://zedd-dashboard-time.netlify.app",
+  },
+];
+
+export const otherProject = [
+  {
+    id: 0,
+    title: "Ztools",
+    desc: "Ztools is simplistic yet highly functional web app that I have created to provide a variety of useful tools.",
+    tech: ["React", "Tailwind", "Vercel"],
+    link: "https://github.com/Zeddnyx/Ztools",
+    live: "https://ztools.vercel.app",
+  },
+  {
+    id: 1,
+    title: "Dashboard App",
+    desc: "A dashboard app built with React and Tailwind CSS. This fully responsive app uses context to manage state, authentication. With a sleek and intuitive user interface.",
+    tech: ["React", "Tailwind", "Context", "Vercel"],
+    link: "https://github.com/Zeddnyx/Dashboard",
+    live: "https://dashboard-zeddnyx.vercel.app",
+  },
+  {
+    id: 2,
+    title: "News App",
+    desc: "News App is challanges from FrontendMentor, and this challanges is to make a grid system with responsive design, and get the data using API from NYTIMES",
+    tech: ["React", "Tailwind", "Netlify"],
+    link: "https://github.com/Zeddnyx/News",
+    live: "https://zedd-news.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Url Shortly",
+    desc: "A URL shortener built with React and API. With this app, you can quickly and easily shorten any URL to make it more manageable and shareable.",
+    tech: ["React", "Tailwind", "Netlify"],
+    link: "https://github.com/Zeddnyx/urlShortly",
+    live: "https://zedd-url-shortly.netlify.app/",
+  },
+];

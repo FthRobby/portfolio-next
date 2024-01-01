289e30f4c02a47367c9ac85799a87dd6fe36fa4c
FthRobby <rbysalam10@gmail.com>
2024-01-01 12:56:25 +0700

    🐛 fix: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,6 @@ import { motion as m } from "framer-motion";
 export default function Footer() {
   return (
     <footer className={mono.className}>
-      {/* Social Mobile */}
       <div className="social-mobile">
         {social.slice(0, 3).map((item, id) => (
           <a key={id} href={item.link} className={`${mono.className} `}>
@@ -14,7 +13,6 @@ export default function Footer() {
           </a>
         ))}
       </div>
-      {/* Social Desktop */}
       <m.div
         initial={{ y: 330 }}
         animate={{ y: 0 }}
@@ -27,17 +25,17 @@ export default function Footer() {
               <a href={item.link} className={`${mono.className} li-nav`}>
                 <item.icon size="25" />
               </a>
-              <span className="tooltip">
-                {/* Tooltip */}
-                {item.title}
-              </span>
+              <span className="tooltip">{item.title}</span>
             </div>
             <div className="h-16 w-px bg-gray1"></div>
           </div>
         ))}
       </m.div>
-
-      {/* Social Desktop Mail */}
+      <m.div>
+        <p className={`${mono.className} text-gray1 hover:text-purple`}>
+          copyright &copy; 2024
+        </p>
+      </m.div>
       <m.div
         initial={{ y: 330 }}
         animate={{ y: 0 }}
